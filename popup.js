58ba7e4693fee01ebf373860a28fc761117de7f9
Chris Michels <chrisjohnmichels@gmail.com>
2022-08-09 20:58:59 -0400

    adding functionality for improved response

--- popup.js
+++ popup.js
@@ -8,13 +8,39 @@ exportCSV.addEventListener("click", async () => {
   
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
-      function: getBearer,
+      function: workoutCount,
     });
   });
   
-  // The body of this function will be executed as a content script inside the
-  // current page
- async function getBearer() {
+async function makeCSV(history) {
+  const replacer = (key, value) => value === null ? '' : value
+  const header = Object.keys(history[0])
+  console.log(header)
+  const csv = [header.join(','),
+      ...history.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','))
+  ].join('\r\n')
+
+  let csvContent = "data:text/csv;charset=utf-8," + csv
+  console.log(csv)
+  var encodedUri = encodeURI(csvContent);
+  var link = document.createElement("a");
+  link.setAttribute("href", encodedUri);
+  link.setAttribute("download", "my_data.csv");
+  document.body.appendChild(link); // Required for FF
+  link.click(); 
+  // var metrics = await data.workoutMetrics
+  // console.log(metrics)
+  // const header2 = Object.keys(metrics)
+  // console.log(header2)
+  // const csv2 = [header2.join(','),
+  //     ...metrics.map(row => header2.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','))
+  // ].join('\r\n')
+
+  // console.log(csv2)
+}
+
+async function workoutCount(){
+    let workouts=[]
     let bearerToken = sessionStorage.getItem('gsvToken') 
     if (!window.location.href.toString().includes('https://accounts.avironactive.com/')){
         if (window.confirm('You must be logged into your Aviron profile to continue. Click OK to be redirected to the Aviron website, Cancel to do nothing'))
@@ -45,34 +71,20 @@ exportCSV.addEventListener("click", async () => {
     "method": "POST"
     });
     let data = await response.json();
-    var history = await data.workoutHistories
+    const history = await data.workoutHistories
+    history.forEach(element =>{
+      try {
+        workouts.push(element.id)
+      } catch (error) {
+        console.log(error)
+      } 
+    });
+    console.log(workouts)
     console.log(history)
-    const replacer = (key, value) => value === null ? '' : value
-    const header = Object.keys(history[0])
-    console.log(header)
-    const csv = [header.join(','),
-        ...history.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','))
-    ].join('\r\n')
-
-    let csvContent = "data:text/csv;charset=utf-8," + csv
-    console.log(csv)
-
-
-
-    var encodedUri = encodeURI(csvContent);
-    var link = document.createElement("a");
-    link.setAttribute("href", encodedUri);
-    link.setAttribute("download", "my_data.csv");
-    document.body.appendChild(link); // Required for FF
-
-    link.click(); 
-    // var metrics = await data.workoutMetrics
-    // console.log(metrics)
-    // const header2 = Object.keys(metrics)
-    // console.log(header2)
-    // const csv2 = [header2.join(','),
-    //     ...metrics.map(row => header2.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','))
-    // ].join('\r\n')
-
-    // console.log(csv2)
+    try {
+      makeCSV(history)
+    } catch (error) {
+      console.log(error)
+    }
+    
 }
